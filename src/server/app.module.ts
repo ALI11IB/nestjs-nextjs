de2714054a6d8520a5eb380af9ee1f1bffe60fe3
ali.la.ibrahem <ali.la.ibrahem@gmail.com>
2022-04-01 03:23:39 -0700

    update appmodule and appcontroller

--- src/server/app.module.ts
+++ src/server/app.module.ts
@@ -1,9 +1,13 @@
 import { Module } from '@nestjs/common';
+import { RenderModule } from 'nest-next';
+import Next from 'next';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 
 @Module({
-  imports: [],
+  /* should pass a NEXT.js server instance
+        as the argument to `forRootAsync` */
+  imports: [RenderModule.forRootAsync(Next({ dev: true }))],
   controllers: [AppController],
   providers: [AppService],
 })
